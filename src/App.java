e660942486934ae8d3e58fb216070965ed7d8efe
casey <cpmoden@gmail.com>
2024-10-08 08:45:44 -0400

    framecount done

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import processing.core.*;
 
 public class App extends PApplet{
+    int frameWhenClicked = -30;
     public static void main(String[] args)  {
         PApplet.main("App");
     }
@@ -10,11 +11,19 @@ public class App extends PApplet{
     }
 
     public void settings(){
-        
+        size(800, 600);
     }
 
     public void draw(){
+        background(255);
+        if(frameWhenClicked + 30 >= frameCount){
+            ellipse(50, 50, 100, 100);
+        }   
        
 
+    }
+
+    public void mousePressed(){
+        frameWhenClicked = frameCount;
     }
 }
